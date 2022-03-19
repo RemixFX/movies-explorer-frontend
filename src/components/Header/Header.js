d4d00f1b45c8e437ee461f28178b254fa075ca76
RemixFX <remix.master@yandex.ru>
2022-03-19 04:36:28 +0300

    feat: add animation to the navigation menu

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,13 +1,14 @@
 import React from "react";
 import logo from '../../images/logo.svg';
 import { Link, useLocation } from 'react-router-dom';
+import { CSSTransition } from 'react-transition-group';
 import Navigation from '../Navigation/Navigation';
 import profileLogo from '../../images/profile-logo.svg'
 
 function Header() {
 
   const location = useLocation();
-  const [classPopup, setClassPopup] = React.useState('')
+  const [classPopup, setClassPopup] = React.useState('');
 
   return (
     <header className={`${location.pathname === '/' ? 'header' : 'header_background_black'}`}>
@@ -21,6 +22,8 @@ function Header() {
           <><button className='header__menu-button' type='button'
             onClick={() => setClassPopup('popup_opened')}></button>
             <div className={`popup ${classPopup}`}>
+            <CSSTransition in={classPopup === 'popup_opened'} classNames="menu-animated"
+             timeout={700}>
               <div className="header__menu">
                 <button className="popup__close-button" type="button"
                 onClick={() => setClassPopup('')}></button>
@@ -30,6 +33,7 @@ function Header() {
                   <img className='header__profile-logo' src={profileLogo} alt="" />
                 </Link>
               </div>
+            </CSSTransition>
             </div>
             <div className='header__buttons header__buttons_type_authorized'>
               <Navigation />
